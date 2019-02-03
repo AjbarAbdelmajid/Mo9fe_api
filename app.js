788a00a3473ba016b542a55261488ec31eaa2388
mjid <ajbareabdlmjid@gmail.com>
2019-02-03 10:33:31 +0100

    initial commit

---
+++ app.js
@@ -0,0 +1,39 @@
+let createError = require('http-errors'),
+    express = require('express'),
+    path = require('path'),
+    cookieParser = require('cookie-parser'),
+    logger = require('morgan'),
+    env = require('dotenv').load();
+
+let app = express();
+//app.set('view engine', 'pug');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+//app.use(express.static(path.join(__dirname, 'public')));
+
+// The main page
+app.get('/', function(req, res) {
+    res.send('Page under construction.');
+});
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+    next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
+});
+
+module.exports = app;
