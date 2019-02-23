ef85c9e61d13685104613f87668ee3d81b7224fd
mjid <ajbareabdlmjid@gmail.com>
2019-02-23 23:41:18 +0100

    [1016] add & delete & list categories

--- routes/sort/privetRoutes.js
+++ routes/sort/privetRoutes.js
@@ -1,7 +1,8 @@
 let express = require('express'),
     router = express.Router();
 
-let cities = require('../../controllers/cities/citiesController');
+let categories = require('../../controllers/categories/categoriesController'),
+    cities = require('../../controllers/cities/citiesController');
 
 // Delete cities
 router.delete('/city/delete/:code_postal', cities.delete_city);
@@ -9,6 +10,12 @@ router.delete('/city/delete/:code_postal', cities.delete_city);
 //add cities
 router.post('/city/add', cities.add_city);
 
+
+// Delete categories
+router.delete('/category/delete/:categories_id', categories.delete_categories);
+
+//add categories
+router.post('/category/add', categories.add_category);
 
 
 module.exports = router;
