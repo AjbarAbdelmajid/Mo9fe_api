6fe55c160515cf5a9b4252d7c03bebf04fd73362
mjid <ajbareabdlmjid@gmail.com>
2019-02-28 03:26:56 +0100

    [Update] Profile Relations

--- models/relationShips.js
+++ models/relationShips.js
@@ -16,4 +16,12 @@ module.exports = function (db) {
     db.user.hasOne(db.profile, {foreignKey:'User_id'});
     db.profile.belongsTo(db.user, {foreignKey:'User_id'});
 
+    //relationship between Profile And Categories it will create a table name profile_categories
+    db.profile.belongsToMany(db.categories, {through:'profile_categories', foreignKey:'profile_id'});
+    db.categories.belongsToMany(db.profile, {through:'profile_categories', foreignKey:'Categories_id'});
+
+    //relationship between Profile And City it will create a table name profile_city
+    db.profile.belongsToMany(db.city, {through:'profile_city', foreignKey:'profile_id'});
+    db.city.belongsToMany(db.profile, {through:'profile_city', foreignKey:'City_id'});
+
 };
