dd92be322b3ed275b59f38a89ebf33e89cb79b28
mjid <ajbareabdlmjid@gmail.com>
2019-03-03 14:57:20 +0100

    [Update] relationships between (announce or profile) and (city, categories)

--- models/relationShips.js
+++ models/relationShips.js
@@ -1,28 +1,28 @@
 module.exports = function (db) {
 
     //relationship between Announce And Categories it will create a table name announce_categories
-    db.announce.belongsToMany(db.categories, {through:'announce_categories', foreignKey:'Announce_id'});
-    db.categories.belongsToMany(db.announce, {through:'announce_categories', foreignKey:'Categories_id'});
+    db.announce.belongsTo(db.categories, {foreignKey:'categories_id'});
+    db.categories.hasMany(db.announce, {foreignKey:'categories_id'});
 
     //relationship between Announce And City it will create a table name announce_city
-    db.announce.belongsToMany(db.city, {through:'announce_city', foreignKey:'Announce_id'});
-    db.city.belongsToMany(db.announce, {through:'announce_city', foreignKey:'City_id'});
+    db.announce.belongsTo(db.city, {foreignKey:'code_postal'});
+    db.city.hasMany(db.announce, {foreignKey:'code_postal'});
 
     //relationship between User And Announce and put user_id in Announce
-    db.user.hasMany(db.announce, {foreignKey:'User_id'});
-    db.announce.belongsTo(db.user, {foreignKey:'User_id'});
+    db.user.hasMany(db.announce, {foreignKey:'user_id'});
+    db.announce.belongsTo(db.user, {foreignKey:'user_id'});
 
     //relationship between User And Profile and put user_id in Profile
     db.user.hasOne(db.profile, {foreignKey:'User_id'});
     db.profile.belongsTo(db.user, {foreignKey:'User_id'});
 
     //relationship between Profile And Categories it will create a table name profile_categories
-    db.profile.belongsToMany(db.categories, {through:'profile_categories', foreignKey:'profile_id'});
-    db.categories.belongsToMany(db.profile, {through:'profile_categories', foreignKey:'Categories_id'});
+    db.profile.belongsTo(db.categories, {foreignKey:'categories_id'});
+    db.categories.hasMany(db.profile, {foreignKey:'categories_id'});
 
     //relationship between Profile And City it will create a table name profile_city
-    db.profile.belongsToMany(db.city, {through:'profile_city', foreignKey:'profile_id'});
-    db.city.belongsToMany(db.profile, {through:'profile_city', foreignKey:'City_id'});
+    db.profile.belongsTo(db.city, {foreignKey:'code_postal'});
+    db.city.hasMany(db.profile, {foreignKey:'code_postal'});
 
     //relationship between files And Announce and put announce_id in to files
     db.announce.hasMany(db.files, {foreignKey:'announce_id'});
